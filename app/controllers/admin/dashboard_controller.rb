136bcb928232fc576e8db429a047cd20c4134b29
meteora277 <justin.s.diaz@gmail.com>
2022-02-02 20:08:48 -0500

    query item count and category count from db

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   require 'dotenv/load'
   http_basic_authenticate_with :name => ENV["USER_NAME"], :password => ENV["PASSWORD"]
   def show
-    
+    @item_count = Product.count(:id)
+    @number_of_categories = Category.count(:id)
   end
 end
