bb18f258c621f160bb7ee3158edfdb679fae5aae
meteora277 <justin.s.diaz@gmail.com>
2022-02-02 19:14:05 -0500

    add user authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  require 'dotenv/load'
+  http_basic_authenticate_with :name => ENV["USER_NAME"], :password => ENV["PASSWORD"]
   def show
+    
   end
 end
