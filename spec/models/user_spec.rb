8be6bad68597ffc1c81241197a8602f0cf543a58
meteora277 <justin.s.diaz@gmail.com>
2022-02-07 18:03:31 -0500

    create password validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation" do 
+    it "Should Validate if a password was provided" do 
+      @user = User.new
+      @user.password = "potatooo"
+      @user.password_confirmation ="potatoo0"
+      @user.save
+      puts @user.errors.full_messages
+    end
+  end
 end
