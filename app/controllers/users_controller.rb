8e040a6679e16ef5329763820a6be58ace55b0ee
meteora277 <justin.s.diaz@gmail.com>
2022-02-07 19:40:37 -0500

    add downcase to user email input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
   def create
     user = User.new(user_params)
+    user.email = user_params[:email].downcase
     if user.save
       session[:user_id] = user.id
       redirect_to "/"
