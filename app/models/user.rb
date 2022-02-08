c3d49a3be97c3782778aae206877e22dd65e1f5f
meteora277 <justin.s.diaz@gmail.com>
2022-02-07 19:41:01 -0500

    add email validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  validates :password, presence: true
+  validates :password, :email, :first_name, :last_name, presence: true
   validates :password_confirmation, presence: true
   validates :password, length: {minimum: 8}
   validates_confirmation_of :password
+  validates :email, :uniqueness => {:case_sensitive => false}
 
 end
