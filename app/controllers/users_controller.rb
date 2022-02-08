6b75ae77b9146d92462de10fbc9c52a02c370a7b
meteora277 <justin.s.diaz@gmail.com>
2022-02-07 22:27:58 -0500

    parse email before insert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.email = user_params[:email].downcase
+    user.parse_email
     if user.save
       session[:user_id] = user.id
       redirect_to "/"
