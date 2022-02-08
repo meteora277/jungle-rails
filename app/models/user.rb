f575dc05926e33976cec380c14c44f85ec4e40b1
meteora277 <justin.s.diaz@gmail.com>
2022-02-07 20:03:51 -0500

    define authenicate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ActiveRecord::Base
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
   has_secure_password
   validates :password, :email, :first_name, :last_name, presence: true
   validates :password_confirmation, presence: true
   validates :password, length: {minimum: 8}
   validates_confirmation_of :password
   validates :email, :uniqueness => {:case_sensitive => false}
+
+
 
 end
